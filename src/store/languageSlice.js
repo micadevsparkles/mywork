346acbc7a29a240816d7fd3735d443743fb39c8b
micadevsparkles <53092907+micadevsparkles@users.noreply.github.com>
2026-04-24 14:32:40 -0300

    Add languageSlice for managing language state

---
+++ src/store/languageSlice.js
@@ -0,0 +1,14 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const languageSlice = createSlice({
+  name: 'language',
+  initialState: { current: 'pt' },
+  reducers: {
+    setLanguage: (state, action) => {
+      state.current = action.payload;
+    },
+  },
+});
+
+export const { setLanguage } = languageSlice.actions;
+export default languageSlice.reducer;
